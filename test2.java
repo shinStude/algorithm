77f65c0f5aa24aa53ad58e88bed30fa11a0e7df3
신재민 <tlswoals87@naver.com>
2024-05-09 15:35:50 +0900

    파일구조 변경 및 2일차 알고리즘 문제 풀이 등록

---
+++ test2.java
@@ -0,0 +1,20 @@
+// import java.util.*;
+
+// class Main{
+//     public void solution(String str){
+//         for(int i=0; i < str.length(); i++) {
+//             if(Character.isUpperCase(str.charAt(i))) {
+//                 System.out.println(str.charAt(i) + "대문자");
+//             } else {
+//                 System.out.println(str.charAt(i) + "소문자");
+//             }
+//         }
+//     }
+
+//     public static void main(String[] args){
+//         Main T = new Main();
+//         Scanner kb = new Scanner(System.in);
+//         String str = kb.next();
+//         T.solution(str);
+//     }
+// }
